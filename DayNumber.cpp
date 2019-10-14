e1831acb60fab9c445f4279b3b51e1baa4c0030b
Kunal Kumar <kunal164107@gmail.com>
2019-10-14 11:42:43 +0530

    Update DayNumber.cpp
    Added question

--- DayNumber.cpp
+++ DayNumber.cpp
@@ -1,3 +1,18 @@
+/*
+Write a program that prints the day number of the year, given the date in the
+form month-day-year. For example, if the input is 1-1-2006, the day
+number is 1; if the input is 12-25-2006, the day number is 359. The program
+should check for a leap year. A year is a leap year if it is divisible by 4, but not
+divisible by 100. For example, 1992 and 2008 are divisible by 4, but not
+by 100. A year that is divisible by 100 is a leap year if it is also divisible by
+400. For example, 1600 and 2000 are divisible by 400. However, 1800 is not
+a leap year because 1800 is not divisible by 400.
+
+You may name the program as you please, for example, "DayNumber.cpp"
+
+Please make sure the program compiles and runs as it should!
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
